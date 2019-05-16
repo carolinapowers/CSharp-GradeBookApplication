3e3432a20659d7ec20277181542d5251c40c37b5
Carolina Powers <carolinapoloni@gmail.com>
2019-05-16 14:05:36 -0400

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Name = name;
+            Type = Enums.GradeBookType.Standard
         }
     }
 }
